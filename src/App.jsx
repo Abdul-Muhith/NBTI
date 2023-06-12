f9b51806aa23cfbd3c33c54f27d33c18044d19f5
Abdul Muhith <amuhith.mern@outlook.com>
2023-06-12 19:00:46 +0600

    frontEND setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,27 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import Registration from './component/registrations'
+import './App.css'
+import Registration from './components/registrations'
 
-const style = {
-  background: '#563D7C',
-  color: '#95C1F3',
-  fontWeight: 'bold',
-  width: '50%',
-  margin: 'auto',
-  padding: '0.6rem',
-  textAlign: 'center'
-}
+// const style = {
+//   background: '#563D7C',
+//   color: '#95C1F3',
+//   fontWeight: 'bold',
+//   width: '50%',
+//   margin: 'auto',
+//   padding: '0.6rem',
+//   textAlign: 'center'
+// }
 
 function App() {
   return (
-    <div className="p-3">
-      <div style={style}>
+    <div className="container">
+      {/* <div style={style}>
         <h1 style={{ color: 'white' }}>N B T I</h1>
         <small>Nishi Banu To Islam</small>
-      </div>
+      </div> */}
       {/* Student Registration */}
       <Registration />
     </div>
